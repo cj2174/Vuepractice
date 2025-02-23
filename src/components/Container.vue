94c6ce14440ebff721668d3ab2162c4f7e594964
cj2174 <cj2174@naver.com>
2025-02-23 19:09:25 +0900

    Vuestagram 프로젝트 생성, 레이아웃 만들기

---
+++ src/components/Container.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    컨테이너에요
+    <Post />
+    <Post />
+    <Post />
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+export default {
+  components: {
+    Post,
+  },
+};
+</script>
+
+<style></style>
